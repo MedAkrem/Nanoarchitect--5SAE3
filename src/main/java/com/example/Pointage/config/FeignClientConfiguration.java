467ac22bd933dc5ca7b254780df0b804c27eb387
oussema-kossentini <oussema.kossentini@esprit.tn>
2024-11-11 10:45:44 +0100

    Initial cot

--- src/main/java/com/example/Pointage/config/FeignClientConfiguration.java
+++ src/main/java/com/example/Pointage/config/FeignClientConfiguration.java
@@ -2,7 +2,7 @@ package com.example.Pointage.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
-
+//
 public class FeignClientConfiguration {
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
